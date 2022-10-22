c5991fba4a203ca2949501ce7042c0c88561451e
AlexandreDev <alexandreagradev@gmail.com>
2022-10-22 02:59:30 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,23 +1,83 @@
-//https://image.tmdb.org/t/p/original/tSxbUnrnWlR5dQvUgqMI7sACmFD.jpg
+const apiUrl = 'https://api.themoviedb.org/3/'
+const apiKey = 'bc212c7fd1b1d2c5bf84e70a32cfd6e2'
+const apiLang = 'pt-BR'
+const imgUrl = 'https://image.tmdb.org/t/p/original'
 
-fetch ("https://api.themoviedb.org/3/trending/all/day?api_key=bc212c7fd1b1d2c5bf84e70a32cfd6e2&language=pt-BR")
+const randomNumber = (size) => {
+    return Math.floor(Math.random() * size)
+}
+
+let type = randomNumber(4);
+type = type % 2 === 0 ? 'movie' : 'tv'
+
+fetch(`${apiUrl}trending/${type}/day?api_key=${apiKey}&language=${apiLang}`)
     .then(response => {
-        return response.json();
+        return response.json()
     })
+
     .then(data => {
-        const randomNumber = Math.floor(Math.random() * 19);
+        let show = randomNumber(data.results.length)
 
-        document.querySelector("#full-page").style.backgroundImage = 'url(https://image.tmdb.org/t/p/original' + data.results[randomNumber].backdrop_path +')';
+        const showName = !data.results[show].title ? data.results[show].name : data.results[show].title
 
-        const lancamento = new Date('jan 1, 2024 09:00:00').getTime();
-        const hoje = new Date().getTime();
+       
+        document.querySelector('#show-name').innerText = '*Imagem de fundo: ' + showName
 
-        const diferenca = lancamento - hoje;
+        const backgroundImg = imgUrl + data.results[show].backdrop_path
 
-        let days = diferenca / (1000 * 60 * 60 * 24);
-
-        let hours = (diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
-        console.log(hours); 
+        document.querySelector('#full-page').style.backgroundImage = 'Linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(' + backgroundImg + ')'
     })
 
-   
+    .catch(erro => {
+        console.log('Error: ' + error.message)
+    })
+
+const launchDate = new Date('Jan 1, 2024 09:00:00').getTime()
+
+const interval = () => {
+    const timeNow = new Date().getTime()
+    const timeDifference = launchDate - timeNow
+    
+    let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+    days = days < 10 ? '0' + days : days
+    console.log(timeDifference)
+
+    let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    hours = hours < 10 ? '0' + hours : hours
+
+    let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+    minutes - minutes < 10 ? '0' + minutes : minutes
+
+    let seconds = Math.floor((timeDifference % (1000 * 60)) / (1000))
+    seconds - seconds < 10 ? '0' + seconds : seconds
+    
+    document.querySelector('#time-left').innerHTML =`
+        <ul>
+            <li>
+                <span class="time-num">${days}</span>
+                <span class="time-txt">dias</span>
+            </li>
+            <li>
+                <span class="time-num">${hours}</span>
+                <span class="time-txt">horas</span>
+            </li>
+            <li>
+                <span class="time-num">${minutes}</span>
+                <span class="time-txt">minutos</span>
+            </li>
+            <li>
+                <span class="time-num">${seconds}</span>
+                <span class="time-txt">segundos</span>
+            </li>
+        </ul>`
+
+    if (timeDifference < 0) {
+        clearInterval(interval)
+        document.querySelector('#time-left').innerHTML = "Tempo Finalizado!"
+    }
+}
+
+setInterval(interval, 1000)
+
+
+
